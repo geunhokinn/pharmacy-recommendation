151e115d7568f1a9e420a9e2afd586cbb77e1428
geunhokinn <gghh1220@gmail.com>
2024-06-28 18:32:24 +0900

    feat: PharmacyRecommendationApplication 추가 - jpa auditing 기능 활성화하기 위해 @EnableJpaAuditing 추가

--- src/main/java/com/example/pharmacyrecommendation/PharmacyRecommendationApplication.java
+++ src/main/java/com/example/pharmacyrecommendation/PharmacyRecommendationApplication.java
@@ -2,12 +2,13 @@ package com.example.pharmacyrecommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // jpa auditing 기능 활성화
 @SpringBootApplication
-public class PharmacyRecommendationApplication {
+public class PharmacyRecommendationApplication extends BaseTimeEntity {
 
     public static void main(String[] args) {
         SpringApplication.run(PharmacyRecommendationApplication.class, args);
     }
-
 }
