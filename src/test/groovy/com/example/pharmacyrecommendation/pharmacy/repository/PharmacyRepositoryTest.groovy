0994abafa990a2467f8ed332fcae1a7038539691
geunhokinn <gghh1220@gmail.com>
2024-06-28 18:42:47 +0900

    test: PharmacyRepositoryTest 추가 - BasTimeEntity 등록 후 Pharmacy 엔트리 매핑 테스트 추가

--- src/test/groovy/com/example/pharmacyrecommendation/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/pharmacyrecommendation/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -4,6 +4,8 @@ import com.example.pharmacyrecommendation.AbstractIntegrationContainerBaseTest
 import com.example.pharmacyrecommendation.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 
+import java.time.LocalDateTime
+
 // 스프링 부트 통합 테스트 -> 상속 받아서 이미 통합 테스트 환경이 구축이 되었다.
 //@SpringBootTest
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
@@ -68,5 +70,27 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
         then:
         result.size() == 1
+    }
+
+    // BasTimeEntity 등록 후 매핑이 되는지 테스트
+    def "BaseTimeEntity 등록"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        // 미리 생성해둔 시간 이후에 엔티티를 저장한 시간인지 검증
+        result.get(0).createdDate.isAfter(now)
+        result.get(0).modifiedDate.isAfter(now)
     }
 }
