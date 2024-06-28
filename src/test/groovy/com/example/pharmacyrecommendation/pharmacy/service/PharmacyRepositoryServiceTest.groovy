62534bead06fd46ee1dc5794bb18a51778c11a8b
geunhokinn <gghh1220@gmail.com>
2024-06-28 17:46:58 +0900

    test: PharmacyRepositoryServiceTest 추가 - 주소 수정에 대한 dirty checking 성공, 실패 테스트 추가

---
+++ src/test/groovy/com/example/pharmacyrecommendation/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -0,0 +1,72 @@
+package com.example.pharmacyrecommendation.pharmacy.service
+
+import com.example.pharmacyrecommendation.AbstractIntegrationContainerBaseTest
+import com.example.pharmacyrecommendation.pharmacy.entity.Pharmacy
+import com.example.pharmacyrecommendation.pharmacy.repository.PharmacyRepository
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Specification
+
+// 테스트 컨테이너를 이용한 스프링 부트 통합 테스트, 주소 업데이트 테스트, dirty checking test
+class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    PharmacyRepositoryService pharmacyRepositoryService
+
+    @Autowired
+    PharmacyRepository pharmacyRepository
+
+    // 모든 테스트 메서드 시작 전에 DB 초기화
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
+
+    // dirty checking 성공 테스트
+    def "PharmacyRepository update - dirty checking success"() {
+        given:
+        String inputAddress = "서울 특별시 성북구 종암동"
+        String modifiedAddress = "서울 광진구 구의동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(inputAddress)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        // 약국 엔티티 저장
+        def entity = pharmacyRepository.save(pharmacy)
+        // 약국 엔티티 주소 수정(dirty checking o)
+        pharmacyRepositoryService.updateAddress(entity.id, modifiedAddress)
+        // 약국 엔티티 주소가 수정되었는지 확인하기 위해 가져오기
+        def result = pharmacyRepository.findAll()
+
+        then:
+        // 약국 엔티티 주소가 수정되었는지 검증
+        result.get(0).pharmacyAddress == modifiedAddress
+    }
+
+    // dirty checking 실패 테스트
+    def "PharmacyRepository update - dirty checking fail"() {
+        given:
+        String inputAddress = "서울 특별시 성북구 종암동"
+        String modifiedAddress = "서울 광진구 구의동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(inputAddress)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        // 약국 엔티티 저장
+        def entity = pharmacyRepository.save(pharmacy)
+        // 약국 엔티티 주소 수정(dirty checking x)
+        pharmacyRepositoryService.updateAddressWithoutTransaction(entity.id, modifiedAddress)
+        // 약국 엔티티 주소가 수정되었는지 확인하기 위해 가져오기
+        def result = pharmacyRepository.findAll()
+
+        then:
+        // 약국 엔티티 주소가 기존 주소랑 같은지 검증
+        result.get(0).pharmacyAddress == inputAddress
+    }
+}
