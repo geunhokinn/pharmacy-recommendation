9a53d4b963cc63e703c1f8ca927db73fd36690c6
geunhokinn <gghh1220@gmail.com>
2024-07-01 12:06:39 +0900

    feat: PharmacyRecommendationService 추가 - 최종적으로 가까운 약국을 찾고 약국 안내(추천)를 저장하기 위해 recommendPharmacyList() 메서드 추가

---
+++ src/main/java/com/example/pharmacyrecommendation/pharmacy/service/PharmacyRecommendationService.java
@@ -0,0 +1,48 @@
+package com.example.pharmacyrecommendation.pharmacy.service;
+
+import com.example.pharmacyrecommendation.api.dto.DocumentDto;
+import com.example.pharmacyrecommendation.api.dto.KakaoApiResponseDto;
+import com.example.pharmacyrecommendation.api.service.KakaoAddressSearchService;
+import com.example.pharmacyrecommendation.direction.entity.Direction;
+import com.example.pharmacyrecommendation.direction.service.DirectionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+// 최종적으로 약국 안내(추천)를 하는 서비스
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacyRecommendationService {
+
+    private final KakaoAddressSearchService kakaoAddressSearchService;
+    private final DirectionService directionService;
+
+    // 최종적으로 가까운 약국을 찾고 약국 안내(추천)를 저장하는 메서드
+    public void recommendPharmacyList(String address) {
+
+        // 고객의 문자열 주소를 카카오 api 를 통해서 위치 기반 데이터로 변경
+        KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
+
+        // retry 가 모두 실패하거나 주소를 검색했는데 위도, 경도가 맵핑되지 않을 때 null 을 반환할 수 있음
+        if(Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentDtoList())) {
+            log.error("[KakaoAddressSearchService recommendPharmacyList fail] Input address: {}", address);
+            return;
+        }
+
+        // documentDtoList 에서 첫 번째 값을 기준으로 사용
+        DocumentDto documentDto = kakaoApiResponseDto.getDocumentDtoList().get(0);
+
+        // 가까운 약국 리스트를 찾기
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
+
+        // 찾은 약국 리스트를 데이터 베이스에 저장
+        directionService.saveAll(directionList);
+    }
+
+}
