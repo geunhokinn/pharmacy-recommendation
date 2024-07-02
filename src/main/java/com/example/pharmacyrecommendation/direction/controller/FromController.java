b8f3f2534135ee50ee0cd83c50ce28d490eab8b2
geunhokinn <gghh1220@gmail.com>
2024-07-02 22:11:31 +0900

    feat: FromController 추가 - main 화면으로 이동하기 위해 컨트롤러 추가

---
+++ src/main/java/com/example/pharmacyrecommendation/direction/controller/FromController.java
@@ -0,0 +1,17 @@
+package com.example.pharmacyrecommendation.direction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class FromController {
+
+    // 컨트롤러에서 요청이 오면 알맞는 페이지로 이동
+    // main 화면으로 이동
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
