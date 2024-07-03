4bebdcf49b77367fd2664ffd5a48c4aa2713e721
geunhokinn <gghh1220@gmail.com>
2024-07-03 21:09:03 +0900

    refactor: FormController 수정 - FromController 에서 FormController 로 오타 수정

--- src/main/java/com/example/pharmacyrecommendation/direction/controller/FormController.java
+++ src/main/java/com/example/pharmacyrecommendation/direction/controller/FormController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
-public class FromController {
+public class FormController {
 
     // 컨트롤러에서 요청이 오면 알맞는 페이지로 이동
     // main 화면으로 이동
