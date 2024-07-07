e9d51a136e39a90b52cfb970fbbfcf60028d6c15
geunhokinn <gghh1220@gmail.com>
2024-07-07 17:13:00 +0900

    feat: PharmacySearchService 추가 - 데이터베이스가 아닌 레디스에 캐싱해둔 약국 데이터를 먼저 조회하기 위해 레디스에서 약국 데이터를 조회하는 로직 추가

--- src/main/java/com/example/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
+++ src/main/java/com/example/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
@@ -1,8 +1,8 @@
 package com.example.pharmacyrecommendation.pharmacy.service;
 
+import com.example.pharmacyrecommendation.pharmacy.cache.PharmacyRedisTemplateService;
 import com.example.pharmacyrecommendation.pharmacy.dto.PharmacyDto;
 import com.example.pharmacyrecommendation.pharmacy.entity.Pharmacy;
-import com.example.pharmacyrecommendation.pharmacy.service.PharmacyRepositoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,14 +16,27 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PharmacySearchService {
 
+    // db 에서 약국 데이터를 조회하기 위해 의존성 주입
     private final PharmacyRepositoryService pharmacyRepositoryService;
+    // redis 에서 약국 데이터를 조회하기 위해 의존성 주입
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
 
     // 약국 데이터를 조회하고 dto 로 반환하는 메서드
     public List<PharmacyDto> searchPharmacyDtoList() {
 
         // redis
+        // 모든 약국 데이터 조회
+        List<PharmacyDto> pharmacyDtoList = pharmacyRedisTemplateService.finalAll();
+        // 약국 데이터가 있으면 redis 에 있는 데이터를 바로 사용
+        if (!pharmacyDtoList.isEmpty()) {
+            log.info("redis findAll success");
+            return pharmacyDtoList;
+        }
+
+        // redis 에 문제가 있거나 장애가 있을 경우 db 를 사용
 
         // db
+        // 모든 약국 데이터 조회
         return pharmacyRepositoryService.findAll()
                 .stream()
                 .map(this::convertTopharmacyDto)
